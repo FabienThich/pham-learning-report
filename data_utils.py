c6af5b817e9b1d4ca3b7eed5e25d9dc8286e33f6
FabienThich <fabienthich30@gmail.com>
2026-01-21 12:29:34 -0500

    add requirements.txt

--- data_utils.py
+++ data_utils.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import streamlit as st
 
-def load_data():
-  df = pd.read_csv(st.secrets['DATASET'])
-  # df['session_date'] = pd.to_datetime[df['session_date']]
 
-  return df
+def load_data():
+    df = pd.read_csv(st.secrets["DATASET"])
+    # df['session_date'] = pd.to_datetime[df['session_date']]
+
+    return df
