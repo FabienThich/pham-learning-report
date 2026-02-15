4ff35e0c519acc88edc58569d04f1d28126caddd
FabienThich <fabienthich30@gmail.com>
2026-02-14 20:27:38 -0500

    fix wordcloud bug

--- eda_utils.py
+++ eda_utils.py
@@ -10,7 +10,9 @@ def individual_data(df: pd.DataFrame, name: str) -> pd.DataFrame:
 def build_report(df: pd.DataFrame):
     student_df = df.groupby("student_name").agg(list).reset_index()
     student_df["average"] = student_df["progress_score"].apply(np.mean) * 10
-    student_df['tutor_notes'] = student_df['tutor_notes'].apply(lambda notes: " ".join(str(n) for n in notes if isinstance(n, str)))
+    student_df['tutor_notes'] = student_df['tutor_notes'].apply(
+        lambda notes: " ".join(str(n) for n in notes if isinstance(n, str) and str(n).lower() != "nan")
+    )
     
     return student_df
     
