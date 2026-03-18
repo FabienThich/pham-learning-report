e6ea53ee55394a1179c63002125caa57be80d1cd
FabienThich <fabienthich06@gmail.com>
2026-03-17 23:29:55 -0400

    test

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ with st.container():
                 st.rerun()
     with col_title:
         st.markdown(
-            f"<h2 style='text-align: center;'> 📘 {student_name.capitalize()} — Progress Overview </h2>",
+            f"<h2 style='text-align: center;'> 📘{student_name.capitalize()} — Progress Overview </h2>",
             unsafe_allow_html=True,
         )
 
